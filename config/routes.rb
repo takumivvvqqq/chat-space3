ea3037d9777bd1cdc1b95358df6f3bf1e4b95f17
takumivvvqqq <57716986+takumivvvqqq@users.noreply.github.com>
2020-01-05 00:05:36 +0900

    Carrierwaveを導入

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ end
 end
