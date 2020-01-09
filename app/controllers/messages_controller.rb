5123b4702a8cd3e849a4d7aee5384cc68cd065e7
takumivvvqqq <57716986+takumivvvqqq@users.noreply.github.com>
2020-01-09 20:47:23 +0900

    flex json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,11 +8,17 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
+    end
+    respond_to do |format|
+      format.html { redirect_to group_messages_path, notice: "メッセージを送信しました" }
+      format.json
     end
   end
   private
@@ -24,5 +30,6 @@ class MessagesController < ApplicationController
   def set_group
     @group = Group.find(params[:group_id])
   end
+
 end
 # resources :messages, only: [:index, :create]
