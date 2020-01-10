df31edcdbd368d30e786f4dc312deec7226aaead
takumivvvqqq <57716986+takumivvvqqq@users.noreply.github.com>
2020-01-10 21:02:27 +0900

    flex

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,18 +1,3 @@
-// $(function(){
-//   $('#new_message').on('submit', function(e){
-//     e.preventDefault();
-//     var formData = new FormData(this);
-//     var url = $(this).attr('action')
-//     $.ajax({
-//       url: url,
-//       type: "POST",
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-//   });
-// })
 $(function(){ 
   function buildHTML(message){
    if ( message.image ) {
