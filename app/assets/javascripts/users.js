d992e1edc3d1214d1d5d3079338e8de9911129ef
takumivvvqqq <57716986+takumivvvqqq@users.noreply.github.com>
2020-01-11 18:37:06 +0900

    flex

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,25 @@
+
+
+// フォームに入力すると発火する
+inputForm.on('keyup', function(){
+  const target = $(this).val();
+  search(target);
+});
+// ajax処理
 $(function() {
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
-    console.log(input);
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function(users) {
+        console.log("成功です");
+      })
+      .fail(function() {
+        console.log("失敗です");
+      });
   });
 });
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,4 @@ has_many :group_users
 has_many :groups, through: :group_users
 has_many :mesaages
 
-def self.search(input, id)
-  return nil if input == ""
-  User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
-end
-
 end
