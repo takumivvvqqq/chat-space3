3de353f87edbb2f552ba93f9d5782c6fd914fda2
takumivvvqqq <57716986+takumivvvqqq@users.noreply.github.com>
2020-01-11 17:54:44 +0900

    イベント時非同期

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,7 @@
+$(function() {
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    console.log(input);
+  });
+});
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ has_many :group_users
 has_many :groups, through: :group_users
 has_many :mesaages
 
+def self.search(input, id)
+  return nil if input == ""
+  User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+end
+
 end
