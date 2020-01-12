f6aa0382a75a73d35eed255caafc44e3510989ed
takumivvvqqq <57716986+takumivvvqqq@users.noreply.github.com>
2020-01-12 17:39:22 +0900

    flex

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -49,8 +49,8 @@ $(function(){
       `</div>`
     };
     return html;
-  };
-  var reloadMessages = function() {
+   };
+   var reloadMessages = function() {
     last_message_id = $('.message:last').data("message-id");
     $.ajax({
       url: "api/messages",
@@ -68,76 +68,13 @@ $(function(){
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $("#new_message")[0].reset();
       $(".form__submit").prop("disabled", false);
+     }
     })
     .fail(function() {
       console.log('error');
     });
-  };
-  setInterval(reloadMessages, 7000);
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
-});
-
-//   function buildHTML(message){
-//    if ( message.image ) {
-//      var html =
-//       `<div class="message" data-message-id=${message.id}>
-//          <div class="upper-message">
-//            <div class="upper-message__user-name">
-//              ${message.user_name}
-//            </div>
-//            <div class="upper-message__date">
-//              ${message.date}
-//            </div>
-//          </div>
-//          <div class="lower-message">
-//            <p class="lower-message__content">
-//              ${message.content}
-//            </p>
-//          </div>
-//          <img src=${message.image} >
-//        </div>`
-//      return html;
-//    } else {
-//      var html =
-//       `<div class="message" data-message-id=${message.id}>
-//          <div class="upper-message">
-//            <div class="upper-message__user-name">
-//              ${message.user_name}
-//            </div>
-//            <div class="upper-message__date">
-//              ${message.date}
-//            </div>
-//          </div>
-//          <div class="lower-message">
-//            <p class="lower-message__content">
-//              ${message.content}
-//            </p>
-//          </div>
-//        </div>`
-//      return html;
-//    };
-//  }
-// $('.js-form').on('submit', function(e){
-//  e.preventDefault();
-//  var formData = new FormData(this);
-//  var url = $(this).attr('action')
-//  $.ajax({
-//    url: url,
-//    type: "POST",
-//    data: formData,
-//    dataType: 'json',
-//    processData: false,
-//    contentType: false
-//  })
-//   .done(function(data){
-//     var html = buildHTML(data);
-//     $('.messages').append(html);
-//     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-//     $('form')[0].reset();
-//   })
-//    .fail(function(){
-//      alert('error');
-//    });
-//    return false;
-//  });
+   };
+   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 5000);
+   }
+  });
