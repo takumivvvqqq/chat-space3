3e3d6500b1ee819e080a0d647eb4f0a214eb6a06
takumivvvqqq <57716986+takumivvvqqq@users.noreply.github.com>
2020-01-16 21:26:52 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -51,12 +51,7 @@ $('#new_message').on('submit', function(e){
     processData: false,
     contentType: false
   })
-  //  .done(function(data){
-  //    var html = buildHTML(data);
-  //    $('.messages').append(html);      
-  //    $('form')[0].reset();
-  //    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-  //  })
+
 })
 
 var reloadMessages = function() {
